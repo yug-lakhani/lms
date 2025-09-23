259902e5efda1d2e191bbd143378f944bd21d453
yug <yuglakhani01@gmail.com>
2025-09-23 17:57:47 +0530

    hhhll

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { getUserData,userEnrolledCourses } from '../controllers/userController.js';
+import { clerkMiddleware } from '@clerk/express';
+
 
 const userRouter = express.Router();
+userRouter.use(clerkMiddleware());
 
 userRouter.get('/data', getUserData);
 userRouter.get('/enrolled-courses', userEnrolledCourses);
