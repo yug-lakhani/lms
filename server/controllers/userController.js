d67754d8ea5f8d05001f646ed362b4640ce4e771
yug <yuglakhani01@gmail.com>
2025-09-23 18:10:52 +0530

    h

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,30 +1,28 @@
 import User from '../models/User.js';
 
 // Get User Data
+
 export const getUserData = async (req, res) => {
-  try {
-    const { userId } = req.auth();   // 👈 call it as a function now
-    console.log("USER ID:", userId);
-
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.json({ success: false, message: "User not found" });
+    try {
+        const userId = req.auth.userId; 
+        const user = await User.findById(userId); 
+        if (!user) {
+            return res.json({success:false, message: "User not found" });
+        }
+        res.json({success:true, user });
+    } catch (error) {
+        return res.json({ success:false, message: error.message });
     }
-    res.json({ success: true, user });
-  } catch (error) {
-    return res.json({ success: false, message: error.message });
-  }
 };
 
 // Users Enrolled Courses with Lecture Links
 export const userEnrolledCourses = async (req, res) => {
-  try {
-    const { userId } = req.auth();
-    const userData = await User.findById(userId).populate("enrolledCourses");
+    try {
+        const userId = req.auth.userId;
+        const userData = await User.findById(userId).populate('enrolledCourses');
 
-    res.json({ success: true, enrolledCourses: userData.enrolledCourses });
-  } catch (error) {
-    return res.json({ success: false, message: error.message });
-  }
+        res.json({success:true, enrolledCourses: userData.enrolledCourses });
+    } catch (error) {
+        return res.json({ success:false, message: error.message });
+    }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import { getUserData,userEnrolledCourses } from '../controllers/userController.js';
-import { clerkMiddleware } from '@clerk/express';
-
 
 const userRouter = express.Router();
-userRouter.use(clerkMiddleware());
 
 userRouter.get('/data', getUserData);
 userRouter.get('/enrolled-courses', userEnrolledCourses);
